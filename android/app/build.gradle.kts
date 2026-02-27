7b8e243319e1f0a74fac07bbf28bb8dd2def8ea2
Zekri Akram <109292318+ZekriDev1@users.noreply.github.com>
2026-02-27 21:44:49 +0000

    Fix main.yml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tilawalock"
+    namespace = "com.zekri.tilawalock"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tilawalock"
+        applicationId = "com.zekri.tilawalock"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
